46b5538d514943e19f290c6511a7c022cfbd7185
Justin Pakzad <justinpak303@gmail.com>
2024-06-02 15:23:06 -0400

    code refactored a bit

--- src/main.py
+++ src/main.py
@@ -7,36 +7,6 @@ from datetime import datetime
 from collections import defaultdict
 
 
-def backup_dir(
-    source_dir: str, backup_dir: str, create_backup_dir: bool = False
-) -> None:
-    """Back up the entire source directory to the backup directory, with an option to create a backup directory."""
-    home_path = Path.home()
-    source_path = home_path.joinpath(source_dir)
-    backup_path = home_path.joinpath(backup_dir)
-    try:
-        if create_backup_dir:
-            if source_path.is_dir() and not backup_path.exists():
-                shutil.copytree(source_path, backup_path)
-            else:
-                print(
-                    "Source path is not a directory or backup directory already exists"
-                )
-        else:
-            if source_path.is_dir() and backup_path.is_dir():
-                for item in source_path.iterdir():
-                    d = backup_path / item.name
-                    if item.resolve() == backup_path.resolve():
-                        continue
-                    elif item.is_dir():
-                        shutil.copytree(item, d, dirs_exist_ok=True)
-                    else:
-                        shutil.copy(item, d)
-        print(f"Backup of {source_path} created successfully at {backup_path}")
-    except Exception as e:
-        print(f"Error backing up directory: {e}")
-
-
 def check_empty(source_path: Path) -> bool:
     """Check if the directory is empty or only contains system files like .DS_Store."""
     return all(item.name == ".DS_Store" for item in source_path.iterdir()) or not any(
@@ -58,73 +28,100 @@ def get_source_path(home_path: str, source_dir: str) -> Path:
     return Path(home_path).joinpath(source_dir)
 
 
-def does_dir_exist(source_path):
+def verify_directory_exists(source_path):
     if not source_path.is_dir():
-        print(f"Directory does not exist: {source_path}")
-        return None
+        raise FileNotFoundError("The source directory {source_path} does not exist.")
     return True
 
 
-def clean_dir_by_suffix(source_dir: str, shallow: bool = False) -> dict:
-    """Organize files by their suffix into respective directories."""
-    source_path = get_source_path(Path.home(), source_dir)
-    files_by_suffix_dict = defaultdict(list)
-    if not does_dir_exist(source_path):
-        return None
-
-    files = source_path.glob("*") if shallow else source_path.rglob("*")
-    for item in files:
-        if item.is_file():
-            file_suffix = item.suffix[1:].lower()
-            for folder, suffixes in file_cats.items():
-                if file_suffix in suffixes:
-                    files_by_suffix_dict[folder].append(item)
-
-    return files_by_suffix_dict
+def compute_file_size_mb(item):
+    size_mb = (item.stat().st_size) / 1000000
+    return size_mb
 
 
-def clean_dir_by_date(
-    source_dir: str, year_only: bool = False, shallow: bool = False
+# def compute_folder_size(items):
+#     if items:
+#         return round(sum(items), 2)
+#     return None
+
+
+def size_validator(file_size):
+    if file_size < 1:
+        return "small_files"
+    elif file_size >= 1 and file_size < 100:
+        return "medium files"
+
+    return "large files"
+
+
+def copy_dir_contents(source_dir: str, backup_dir: str) -> None:
+    home_path = Path.home()
+    source_path = home_path.joinpath(source_dir)
+    backup_path = home_path.joinpath(backup_dir)
+
+    if not source_path.is_dir():
+        raise FileNotFoundError("The source directory {source_path} does not exist.")
+
+    if not backup_path.exists():
+        backup_path.mkdir(exist_ok=True)
+        print(f"Backup of {source_path} created successfully at {backup_path}")
+
+    for item in source_path.iterdir():
+        destination = backup_path / item.name
+        if item.resolve() == backup_path.resolve():
+            continue
+        elif item.is_dir():
+            shutil.copytree(item, destination, dirs_exist_ok=True)
+        else:
+            shutil.copy(item, destination)
+
+
+def backup_dir(source_dir: str, backup_dir: str) -> None:
+    """Back up the entire source directory to the backup directory, with an option to create a backup directory."""
+    home_path = Path.home()
+    source_path = home_path.joinpath(source_dir)
+    backup_path = home_path.joinpath(backup_dir)
+    try:
+        copy_dir_contents(source_path, backup_path)
+    except Exception as e:
+        print(f"Error backing up directory: {e}")
+
+
+def get_files(source_path: str, shallow: bool = False):
+    return source_path.glob("*") if shallow else source_path.rglob("*")
+
+
+def organize_files(
+    source_dir: str, method="suffix", shallow: bool = False, year_only=False
 ) -> dict:
-    """Organize files by their creation date into yearly or monthly directories."""
     source_path = get_source_path(Path.home(), source_dir)
-    files_by_date_dict = defaultdict(list)
-    does_dir_exist(source_path)
-
-    files = source_path.glob("*") if shallow else source_path.rglob("*")
-    for item in files:
-        if item.is_file():
-            creation_date = datetime.fromtimestamp(item.stat().st_birthtime)
-            if year_only:
-                year = creation_date.year
-                files_by_date_dict[str(year)].append(item)
-            else:
-                year_month = (
-                    f"{calendar.month_name[creation_date.month]}_{creation_date.year}"
-                )
-                files_by_date_dict[year_month].append(item)
-    return files_by_date_dict
-
-
-def clean_dir_by_size(source_dir: str, shallow: bool = False) -> dict:
-    """Categorize files by size into small, medium, or large."""
-    source_path = get_source_path(Path.home(), source_dir)
-    files_by_size_dict = defaultdict(list)
-
-    if not does_dir_exist(source_path):
+    if not verify_directory_exists(source_path):
         return None
 
-    files = source_path.glob("*") if shallow else source_path.rglob("*")
+    files = get_files(source_path, shallow)
+    organized_files = defaultdict(list)
     for item in files:
         if item.is_file():
-            mb = round((item.stat().st_size) / 1000000, 2)
-            if mb < 1:
-                files_by_size_dict["small_files"].append(item)
-            elif mb >= 1 and mb < 100:
-                files_by_size_dict["medium_files"].append(item)
+            if method == "suffix":
+                file_suffix = item.suffix[1:].lower()
+                for folder, suffixes in file_cats.items():
+                    if file_suffix in suffixes:
+                        organized_files[folder].append(item)
+            elif method == "date":
+                creation_date = datetime.fromtimestamp(item.stat().st_birthtime)
+                if year_only:
+                    year = creation_date.year
+                    organized_files[str(year)].append(item)
+                else:
+                    year_month = f"{calendar.month_name[creation_date.month]}_{creation_date.year}"
+                    organized_files[year_month].append(item)
+            elif method == "size":
+                file_size_mb = compute_file_size_mb(item)
+                organized_files[size_validator(file_size_mb)].append(item)
+
             else:
-                files_by_size_dict["large_files"].append(item)
-    return files_by_size_dict
+                print("Invalid method")
+    return organized_files
 
 
 def move_files_to_dir(source_dir: str, files_dict: dict[list]) -> None:
@@ -139,45 +136,59 @@ def move_files_to_dir(source_dir: str, files_dict: dict[list]) -> None:
     delete_empty_dirs(source_path)
 
 
+def print_memory_saved(memory_list):
+    if not memory_list:
+        print("No files were deleted")
+    else:
+        total_memory = sum(memory_list)
+        if total_memory >= 1000:
+            print(
+                f"{len(memory_list)} files have been deleted and {round(total_memory / 1000,2)} GB have been made available."
+            )
+        else:
+            print(
+                f"{len(memory_list)} files have been deleted and {round(total_memory,2)} MB have been made available."
+            )
+
+
+def time_validator(creation_date, n_days=None, n_months=None, n_years=None):
+    now = datetime.now()
+    time_diff = now - creation_date
+    if n_days:
+        if time_diff.days >= n_days:
+            return True
+    if n_months:
+        if (time_diff.days / 30) >= n_months:
+            return True
+    if n_years:
+        if (time_diff.days / 365) >= n_years:
+            return True
+
+    return False
+
+
 def delete_files_by_time(
     source_dir: str, n_days: int = None, n_months: int = None, n_years: int = None
 ) -> None:
     """Delete files that are older than the specified number of days."""
     source_path = get_source_path(Path.home(), source_dir)
 
-    if not does_dir_exist(source_path):
+    if not verify_directory_exists(source_path):
         return None
 
-    files = source_path.rglob("*")
+    files = get_files(source_path)
     memory_saved = []
     for item in files:
         if item.is_file():
             creation_date = datetime.fromtimestamp(item.stat().st_birthtime)
-            time_diff = datetime.now() - creation_date
-            mb = (item.stat().st_size) / 1000000
-            if n_days:
-                if time_diff.days >= n_days:
-                    memory_saved.append(mb)
-                    item.unlink()
-            elif n_months:
-                if (time_diff.days / 30) >= n_months:
-                    memory_saved.append(mb)
-                    item.unlink()
-            elif n_years:
-                if (time_diff.days / 365) >= n_years:
-                    memory_saved.append(mb)
-                    item.unlink()
+            file_size_mb = compute_file_size_mb(item)
+            if time_validator(creation_date, n_days, n_months, n_years):
+                item.unlink()
+                memory_saved.append(file_size_mb)
             else:
-                print("Time period not specified")
+                continue
 
-    if sum(memory_saved) >= 1000:
-        print(
-            f"{len(memory_saved)} files have been deleted and {round(sum(memory_saved) / 1000,2)} GB have been made available."
-        )
-    else:
-        print(
-            f"{len(memory_saved)} files have been deleted and {round(sum(memory_saved),2)} MB have been made available."
-        )
+    print_memory_saved(memory_saved)
     delete_empty_dirs(source_path)
 
 
@@ -201,54 +212,48 @@ def confirm_deletion(
 def get_args():
     parent_parser = argparse.ArgumentParser(add_help=False)
     parent_parser.add_argument(
-        "--backup", action="store_true", help="Create a backup before making changes"
+        "--backup",
+        action="store_true",
+        help="Create a backup before making changes",
+        required=False,
     )
     parent_parser.add_argument(
-        "--backup_dir", help="The directory where the backup will be stored"
+        "--backup_dir",
+        help="The directory where the backup will be stored",
+        required=False,
     )
 
     parser = argparse.ArgumentParser(description="Folder cleanser")
     subparsers = parser.add_subparsers(dest="command", help="Sub-command help")
 
-    suffix_parser = subparsers.add_parser("clean_by_suffix", parents=[parent_parser])
-    suffix_parser.add_argument(
-        "--source_dir", help="The directory to clean", required=True
+    organize_parser = subparsers.add_parser("clean", parents=[parent_parser])
+    organize_parser.add_argument(
+        "--source_dir", help="The directory you would like to clean."
     )
-    suffix_parser.add_argument(
+    organize_parser.add_argument(
+        "--method",
+        help="The method of cleaning",
+        choices=["suffix", "date", "size"],
+        required=True,
+    )
+    organize_parser.add_argument(
         "--shallow",
         help="Perform a shallow clean (do not traverse subdirectories)",
         action="store_true",
+        required=False,
     )
-
-    date_parser = subparsers.add_parser("clean_by_date", parents=[parent_parser])
-    date_parser.add_argument(
-        "--source_dir", help="The directory to clean", required=True
-    )
-
-    date_parser.add_argument(
+    organize_parser.add_argument(
         "--year_only",
-        help="Clean directory by year only rather than the default month and year",
+        help="Option to sort by year only",
         action="store_true",
-    )
-    date_parser.add_argument(
-        "--shallow",
-        help="Perform a shallow clean (do not traverse subdirectories)",
-        action="store_true",
-    )
-
-    size_parser = subparsers.add_parser("clean_by_size", parents=[parent_parser])
-    size_parser.add_argument(
-        "--source_dir", help="The directory to clean", required=True
-    )
-    size_parser.add_argument(
-        "--shallow",
-        help="Perform a shallow clean (do not traverse subdirectories)",
-        action="store_true",
+        required=False,
     )
 
     delete_files_parser = subparsers.add_parser("delete_files", parents=[parent_parser])
     delete_files_parser.add_argument(
-        "--source_dir", help="The directory to clean", required=True
+        "--source_dir",
+        help="The directory where the files you would like to delete are located.",
+        required=True,
     )
     delete_files_time_group = delete_files_parser.add_mutually_exclusive_group(
         required=True
@@ -276,20 +281,18 @@ def main():
     args = parser.parse_args()
     if args.backup and args.backup_dir:
         if confirm_backup(args.source_dir):
-            backup_dir(args.source_dir, args.backup_dir, args.create_backup_dir)
+            backup_dir(args.source_dir, args.backup_dir)
+    if args.command == "clean":
+        if confirm_cleaning(args.source_dir):
+            organized_files = organize_files(
+                source_dir=args.source_dir,
+                method=args.method,
+                shallow=args.shallow,
+                year_only=args.year_only,
+            )
 
-    if args.command == "clean_by_suffix":
-        if confirm_cleaning(args.source_dir):
-            suffix_dict = clean_dir_by_suffix(args.source_dir, args.shallow)
-            move_files_to_dir(args.source_dir, suffix_dict)
-    elif args.command == "clean_by_date":
-        if confirm_cleaning(args.source_dir):
-            date_dict = clean_dir_by_date(args.source_dir, args.year_only, args.shallow)
-            move_files_to_dir(args.source_dir, date_dict)
-    elif args.command == "clean_by_size":
-        if confirm_cleaning(args.source_dir):
-            size_dict = clean_dir_by_size(args.source_dir, args.shallow)
-            move_files_to_dir(args.source_dir, size_dict)
+            move_files_to_dir(args.source_dir, organized_files)
+
     elif args.command == "delete_files":
         if confirm_deletion(args.source_dir):
             if args.n_days:
